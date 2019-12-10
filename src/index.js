161bc7251b727d5b5bfd3b65e6a58794ce52fe37
ericyang <ericyangchen@foxmail.com>
2019-12-10 10:44:09 +0800

    change api index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 import IRouter from './router'
 import * as serviceWorker from './serviceWorker';
+import Utils from './utils/utils'
+import Http from './api'
+window.$utils = Utils
+window.$http = Http
 
 ReactDOM.render(<IRouter />, document.getElementById('root'));
 
